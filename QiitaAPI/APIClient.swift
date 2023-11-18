0ef0e16b7a2b528046f1eb378de17822ebacaeca
Ryuga <ribereo.minami@gmail.com>
2023-11-18 20:09:20 +0900

    feat: #1 APIクライアントを作成。

--- QiitaAPI/APIClient.swift
+++ QiitaAPI/APIClient.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+protocol APIClientInterface {
+    func fetchQiitaItem() async throws -> [QiitaItem]
+}
+
+class APIClient: APIClientInterface {
+    func fetchQiitaItem() async throws -> [QiitaItem] {
+        let (data, response) = try await URLSession.shared.data(from: URL(string: "https://qiita.com/api/v2/tags/swift/items")!)
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw APIError.networkError
+        }
+        switch httpResponse.statusCode {
+        case 200..<300:
+            guard let decodedResponse = try? JSONDecoder().decode([QiitaItem].self, from: data) else {
+                throw APIError.decodeError
+            }
+            return decodedResponse
+        default:
+            throw APIError.unknownError
+        }
+    }
+}
