c8034143a86b34adaa341cafb574ba2d42bcb1f6
Ryuga <ribereo.minami@gmail.com>
2023-11-18 13:16:23 +0900

    Initial Commit

---
+++ QiitaAPI/QiitaAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaAPIApp.swift
+//  QiitaAPI
+//
+//  Created by Ryuga on 2023/11/18.
+//
+
+import SwiftUI
+
+@main
+struct QiitaAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
