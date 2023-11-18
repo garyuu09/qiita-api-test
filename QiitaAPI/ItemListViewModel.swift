b9f0abc4a7a2108f50e0b55bd8d9b6b412cef950
Ryuga <ribereo.minami@gmail.com>
2023-11-18 20:11:12 +0900

    feat: #1 ViewModelを定義した。

--- QiitaAPI/ItemListViewModel.swift
+++ QiitaAPI/ItemListViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+@MainActor
+final class ItemListViewModel: ObservableObject {
+    @Published var items: [QiitaItem] = []
+    private let apiClient: APIClientInterface
+
+    init(apiClient: APIClientInterface) {
+        self.apiClient = apiClient
+    }
+
+    func fetch() async throws {
+        Task {
+            do {
+                items = try await apiClient.fetchQiitaItem()
+            } catch {
+                throw APIError.responseError
+            }
+        }
+    }
+}
