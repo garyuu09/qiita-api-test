d253596402105756f98d5dff89b4d0889f75f39c
Ryuga <ribereo.minami@gmail.com>
2023-11-18 20:09:45 +0900

    feat: #1 エラーを定義した。

--- QiitaAPI/APIError.swift
+++ QiitaAPI/APIError.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+enum APIError: Error {
+    case invalidURL
+    case networkError
+    case responseError
+    case decodeError
+    case unknownError
+
+    var title: String {
+        switch self {
+        case .invalidURL:
+            return "Invalid URL Error"
+        case .networkError:
+            return "Network Error"
+        case .responseError:
+            return "Response Error"
+        case .decodeError:
+            return "Decode Error"
+        default:
+            return "Unknown Error"
+        }
+    }
+}
